b114ec5e64ab4021c55ab6e14e2307edc6ada560
krushnakant-borude <krushnaborude9007@gmail.com>
2025-11-06 18:04:55 +0530

    code skeleton updated exception logging s3 code

---
+++ Xray/components/data_ingestion.py
@@ -0,0 +1,21 @@
+import os,sys
+from Xray.exception import XRayException
+from Xray.logger import logging
+
+
+
+class DataIngestion():
+    def __init__(self):
+        pass
+
+    def get_data_from_s3(self):
+        try:
+            pass
+        except Exception as e:
+            raise XRayException(e,sys)
+        
+    def initiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            raise XRayException(e,sys)
